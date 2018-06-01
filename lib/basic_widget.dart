d76d3523dd455c0b9da7b78bb3895f8b9ad13052
JDNew <623893416@qq.com>
2018-06-01 16:02:18 +0800

    根据手机方向调整GridView的列数

--- lib/basic_widget.dart
+++ lib/basic_widget.dart
@@ -3,21 +3,29 @@ import 'package:flutter/material.dart';
 class BasicWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-  return new DefaultTabController(length: 3, child: new Scaffold(
-    appBar: new AppBar(
-      bottom: new TabBar(tabs: [
-        new Tab(icon: new Icon(Icons.directions_car),),
-        new Tab(icon: new Icon(Icons.directions_boat),),
-        new Tab(icon: new Icon(Icons.directions_bike),)
-      ]),
-      title: new Text("Basic Widget"),
-    ),
-    body: new TabBarView(children: [
-      new BasicColumnWidget(),
-      new Icon(Icons.directions_boat),
-      new Icon(Icons.directions_bike)
-    ]),
-  ));
+    return new DefaultTabController(
+        length: 3,
+        child: new Scaffold(
+          appBar: new AppBar(
+            bottom: new TabBar(tabs: [
+              new Tab(
+                icon: new Icon(Icons.directions_car),
+              ),
+              new Tab(
+                icon: new Icon(Icons.directions_boat),
+              ),
+              new Tab(
+                icon: new Icon(Icons.directions_bike),
+              )
+            ]),
+            title: new Text("Basic Widget"),
+          ),
+          body: new TabBarView(children: [
+            new BasicColumnWidget(),
+            new OrientationList(),
+            new Icon(Icons.directions_bike)
+          ]),
+        ));
   }
 }
 
@@ -39,5 +47,27 @@ class BasicColumnWidget extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+///
+/// 根据手机方向调整GridView的列数
+class OrientationList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new OrientationBuilder(builder: (context, orientation) {
+      return new GridView.count(
+        crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+        children: new List.generate(100, (index) {
+          return new Center(
+            child: new Text(
+              "Item $index",
+              style: Theme.of(context).textTheme.headline,
+            ),
+          );
+        }),
+      );
+    });
   }
 }
